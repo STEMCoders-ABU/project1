d2da9430d4cb8013695ade069a64082cfdcd4af1
Stem Coders <stemcoders.abu@gmail.com>
2020-03-05 13:21:11 +0100

    edited the project

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,1 +1,33 @@
-<h1>WELCOME! I simply changed you</h1>
+<div class="index-header m-n3 mb-5">
+    <div class="translucent-bg-50 text-center headline">
+        <h1 class="index-header-title">Campus Space</h1>
+        <div class="mt-5 auth-container">
+            <a href="<?= site_url(); ?>" class="btn btn-outline-light btn-lg mr-2 shadow shadow-sm" id="login">Sign In</a>
+            <a href="<?= site_url(); ?>" class="btn btn-success btn-lg shadow shadow-sm" id="register">Create Account</a>
+        </div>
+    </div>
+</div>
+
+<div class="index-contents mt-5">
+    <div class="jumbotron shadow shadow-sm">
+        <h4 class="text-center text-dark text-muted">Popular Resources</h4>
+        <hr class="bg-muted w-25">
+
+        <div class="card-deck mt-4">
+            <div class="card">
+                <img class="card-img-top img-fluid" alt="Resource Cover Photo" src="<?= base_url('assets/imgs/header.jpg'); ?>">
+                <div class="card-header t">
+                    <h5>Resource Title</h5>
+                    <div class="text-muted">
+                        <h6>Department of </h6>
+                        <small>Course Title</small>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    
+                </div>
+            </div><!-- /card -->
+        </div>
+    </div>
+</div>
