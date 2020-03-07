94d15ff279640cb64e8655d2c245bcc2c941ed04
Stem Coders <stemcoders.abu@gmail.com>
2020-03-07 06:23:32 +0100

    added popular resources cards
    I also changed the base url to localhost. If you need to test on your mobile device, you have to chnage this your IP address

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -13,7 +13,7 @@
         <h4 class="text-center text-dark text-muted">Popular Resources</h4>
         <hr class="bg-muted w-25">
 
-        <div class="card-deck mt-4">
+        <div class="card-deck mt-5">
             <div class="card">
                 <img class="card-img-top img-fluid" alt="Resource Cover Photo" src="<?= base_url('assets/imgs/header.jpg'); ?>">
                 <div class="card-header t">
@@ -25,7 +25,43 @@
                 </div>
 
                 <div class="card-body">
-                    
+                    <p class="lead text-left alex-font">
+                        A short description of the resource...
+                    </p>
+                </div>
+            </div><!-- /card -->
+
+            <div class="card">
+                <img class="card-img-top img-fluid" alt="Resource Cover Photo" src="<?= base_url('assets/imgs/header.jpg'); ?>">
+                <div class="card-header t">
+                    <h5>Resource Title</h5>
+                    <div class="text-muted">
+                        <h6>Department of </h6>
+                        <small>Course Title</small>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <p class="lead text-left alex-font">
+                        A short description of the resource...
+                    </p>
+                </div>
+            </div><!-- /card -->
+
+            <div class="card">
+                <img class="card-img-top img-fluid" alt="Resource Cover Photo" src="<?= base_url('assets/imgs/header.jpg'); ?>">
+                <div class="card-header t">
+                    <h5>Resource Title</h5>
+                    <div class="text-muted">
+                        <h6>Department of </h6>
+                        <small>Course Title</small>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <p class="lead text-left alex-font">
+                        A short description of the resource...
+                    </p>
                 </div>
             </div><!-- /card -->
         </div>
