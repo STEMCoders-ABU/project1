03e2633b1b01880c3065b65a409b5b1cd6d51bd9
Stem Coders <stemcoders.abu@gmail.com>
2020-03-07 07:04:41 +0100

    Update index.php

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -10,7 +10,7 @@
 
 <div class="index-contents mt-5">
     <div class="jumbotron shadow shadow-sm">
-        <h4 class="text-center text-dark text-muted">Popular Resources</h4>
+        <h4 class="text-center text-dark text-muted">Latest Resources</h4>
         <hr class="bg-muted w-25">
 
         <div class="card-deck mt-5">
@@ -76,6 +76,75 @@
                     <a href="<?= site_url(); ?>" class="btn btn-success btn-block">Explore</a>
                 </div>
             </div><!-- /card -->
+        </div><!-- /card-deck -->
+
+        <div class="card-deck mt-md-5">
+            <div class="card">
+                <img class="card-img-top img-fluid" alt="Resource Cover Photo" src="<?= base_url('assets/imgs/header.jpg'); ?>">
+                <div class="card-header t">
+                    <h5>Resource Title</h5>
+                    <div class="text-muted">
+                        <h6>Department of </h6>
+                        <small>Course Title</small>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <p class="lead text-left alex-font">
+                        A short description of the resource...
+                    </p>
+                </div>
+
+                <div class="card-footer">
+                    <a href="<?= site_url(); ?>" class="btn btn-success btn-block">Explore</a>
+                </div>
+            </div><!-- /card -->
+
+            <div class="card">
+                <img class="card-img-top img-fluid" alt="Resource Cover Photo" src="<?= base_url('assets/imgs/header.jpg'); ?>">
+                <div class="card-header t">
+                    <h5>Resource Title</h5>
+                    <div class="text-muted">
+                        <h6>Department of </h6>
+                        <small>Course Title</small>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <p class="lead text-left alex-font">
+                        A short description of the resource...
+                    </p>
+                </div>
+
+                <div class="card-footer">
+                    <a href="<?= site_url(); ?>" class="btn btn-success btn-block">Explore</a>
+                </div>
+            </div><!-- /card -->
+
+            <div class="card">
+                <img class="card-img-top img-fluid" alt="Resource Cover Photo" src="<?= base_url('assets/imgs/header.jpg'); ?>">
+                <div class="card-header t">
+                    <h5>Resource Title</h5>
+                    <div class="text-muted">
+                        <h6>Department of </h6>
+                        <small>Course Title</small>
+                    </div>
+                </div>
+
+                <div class="card-body">
+                    <p class="lead text-left alex-font">
+                        A short description of the resource...
+                    </p>
+                </div>
+
+                <div class="card-footer">
+                    <a href="<?= site_url(); ?>" class="btn btn-success btn-block">Explore</a>
+                </div>
+            </div><!-- /card -->
+        </div><!-- /card-deck -->
+
+        <div class="mt-5">
+            <a href="<?= site_url(); ?>" class="btn btn-dark stretch-on-hover">Show More <span class="fas fa-arrow-right ml-3"></a></a>
         </div>
     </div>
 
