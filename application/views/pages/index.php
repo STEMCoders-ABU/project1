350010a3d5959047d4f492135f6858e090814e09
lekenz <sheriffdeenyusuf1130@gmail.com>
2020-03-17 10:50:03 +0100

    Revert "Revert "i changed the resoursew""
    This reverts commit ce092437f9a2f1dc1ccb92e560d1f7ad02cfcbef.

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,4 +1,4 @@
-<div class="index-header m-n3 mb-5">
+<!--div class="index-header m-n3 mb-5">
     <div class="translucent-bg-50 text-center headline">
         <h1 class="index-header-title">Campus Space</h1>
         <div class="mt-5 auth-container">
@@ -6,7 +6,95 @@
             <a href="<?= site_url(); ?>" class="btn btn-success btn-lg shadow shadow-sm" id="register">Create Account</a>
         </div>
     </div>
-</div>
+</div --> 
+
+ <!-- Start Slider Area -->
+ <div id="home" class="slider-area">
+    <div class="bend niceties preview-2">
+      <div id="ensign-nivoslider" class="slides">
+        <img src="<?php echo base_url('assets/imgs/slider/slider1.jpg'); ?>"  title="#slider-direction-1" />
+        <img src="<?php echo base_url('assets/imgs/slider/slider2.jpg'); ?>" title="#slider-direction-2" />
+        <img src="<?php echo base_url('assets/imgs/slider/slider3.jpg'); ?>" title="#slider-direction-3" />
+      </div>
+
+      <!-- direction 1 -->
+      <div id="slider-direction-1" class="slider-direction slider-one">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <!-- layer 1 -->
+                <div class="layer-1-1 hidden-xs wow slideInDown" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">The Best Accadmic Information </h2>
+                </div>
+                <!-- layer 2 -->
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <h1 class="title2">We're In The concern Of Helping You get campus information </h1>
+                </div>
+                <!-- layer 3 -->
+                <div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <a class="ready-btn right-btn page-scroll" href="#services">Login</a>
+                  <a class="ready-btn page-scroll" href="#about">See Resources</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- direction 2 -->
+      <div id="slider-direction-2" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content text-center">
+                <!-- layer 1 -->
+                <div class="layer-1-1 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">The Best Accadmic Information </h2>
+                </div>
+                <!-- layer 2 -->
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <h1 class="title2">We're In The concern Of Helping You get campus information</h1>
+                </div>
+                <!-- layer 3 -->
+                <div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <a class="ready-btn right-btn page-scroll" href="#services">See Resource</a>
+                  <a class="ready-btn page-scroll" href="#about">Explore</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- direction 3 -->
+      <div id="slider-direction-3" class="slider-direction slider-two">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="slider-content">
+                <!-- layer 1 -->
+                <div class="layer-1-1 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <h2 class="title1">The Best business Information </h2>
+                </div>
+                <!-- layer 2 -->
+                <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
+                  <h1 class="title2">Helping Business Security  & Peace of Mind for Your Family</h1>
+                </div>
+                <!-- layer 3 -->
+                <div class="layer-1-3 hidden-xs wow slideInUp" data-wow-duration="2s" data-wow-delay=".2s">
+                  <a class="ready-btn right-btn page-scroll" href="#services">See Services</a>
+                  <a class="ready-btn page-scroll" href="#about">Learn More</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Slider Area -->
+
 
 <div class="index-contents mt-5">
     <div class="jumbotron shadow shadow-sm">
