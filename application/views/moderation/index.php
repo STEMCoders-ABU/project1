308ccaa44837eaeccf694462d9f9005e26ace810
Stem Coders <stemcoders.abu@gmail.com>
2020-03-30 07:55:53 +0100

    added the moderation index page

--- application/views/moderation/index.php
+++ application/views/moderation/index.php
@@ -1,3 +1,11 @@
-<div class="mt-5">
+<div class="mt-5 container-fluid text-center">
+    <h4>Moderation Options</h4>
+    <hr class="bg-dark w-25">
 
+    <div class="card shadow shadow-lg mt-5 m-2 m-md-5 p-5 moderation-index-card">
+        <a href="<?= site_url('moderation/add_resource') ?>" class="btn btn-success btn-lg btn-block mb-1">Add Resources</a>
+        <a href="<?= site_url('moderation/manage_resource') ?>" class="btn btn-success btn-lg btn-block mb-1">Resource Management</a>
+        <a href="<?= site_url('moderation/add_news') ?>" class="btn btn-success btn-lg btn-block mb-1">Add News/Updates</a>
+        <a href="<?= site_url('moderation/manage_news') ?>" class="btn btn-success btn-lg btn-block">News/Update Management</a>
+    </div>
 </div>
