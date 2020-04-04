3c878825028a852ef82598d2ffec28680f5c8520
Stem Coders <stemcoders.abu@gmail.com>
2020-04-04 09:39:42 +0100

    updated moderation pages

--- application/views/moderation/index.php
+++ application/views/moderation/index.php
@@ -1,8 +1,28 @@
 <div class="mt-5 container-fluid text-center">
-    <h4>Moderation Options</h4>
-    <hr class="bg-dark w-25">
+    
+    <div class="search-bar card jumbotron mx-auto p-5 shadow shadow-lg mt-5 container">
+        <?= form_open('moderation/add_course', ''); ?>
+            <div class="input-group mb-2"> 
+                <select class="form-control bg-light" name="level" required> 
+                    <option value="l1">100</option>
+                    <option value="l2">200</option>
+                    <option value="l3">300</option>
+                    <option value="l4">400</option>
+                </select>
+            </div>
 
-    <div class="card shadow shadow-lg mt-5 m-2 m-md-5 p-5 moderation-index-card">
+            <div class="input-group mt-3"> 
+                <input type="text" class="form-control bg-light" name="search" placeholder="Enter Course Code" required>
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-success btn-theme">
+                        <span class="fas fa-plus mr-1"></span> Add Course
+                    </button>                          
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="card mt-5 m-2 m-md-5 p-5 moderation-index-card">
         <a href="<?= site_url('moderation/add_resource') ?>" class="btn btn-success btn-lg btn-block mb-1">Add Resources</a>
         <a href="<?= site_url('moderation/manage_resource') ?>" class="btn btn-success btn-lg btn-block mb-1">Resource Management</a>
         <a href="<?= site_url('moderation/add_news') ?>" class="btn btn-success btn-lg btn-block mb-1">Add News/Updates</a>
