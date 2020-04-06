081f386e89b83c6a7fd2989ce2d335393051ddae
Stem Coders <stemcoders.abu@gmail.com>
2020-04-06 17:13:38 +0100

    Update campus_space.sql

--- assets/campus_space.sql
+++ assets/campus_space.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2020 at 04:49 PM
+-- Generation Time: Apr 06, 2020 at 06:12 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.2
 
@@ -44,9 +44,23 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `courses` (
   `id` int(10) UNSIGNED NOT NULL,
+  `department_id` int(10) UNSIGNED NOT NULL,
+  `level_id` int(11) NOT NULL,
   `course_code` varchar(12) NOT NULL,
   `course_title` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `department_id`, `level_id`, `course_code`, `course_title`) VALUES
+(2, 1, 1, 'TEST101', 'Test101 Title'),
+(3, 1, 1, 'TEST102', 'Test102 Title'),
+(4, 2, 3, 'TEST201', 'Test201 Title'),
+(5, 2, 3, 'TEST202', 'Test202 Title'),
+(6, 2, 3, 'TEST203', 'Test203 Title'),
+(7, 2, 3, 'TEST204', 'Test204 Title');
 
 -- --------------------------------------------------------
 
@@ -65,7 +79,8 @@ CREATE TABLE `departments` (
 --
 
 INSERT INTO `departments` (`id`, `department`, `faculty_id`) VALUES
-(1, 'Test Department', 1);
+(1, 'Test Department', 1),
+(2, 'Another Test Department', 2);
 
 -- --------------------------------------------------------
 
@@ -83,7 +98,8 @@ CREATE TABLE `faculties` (
 --
 
 INSERT INTO `faculties` (`id`, `faculty`) VALUES
-(1, 'Test Faculty');
+(1, 'Test Faculty'),
+(2, 'Another Test Faculty');
 
 -- --------------------------------------------------------
 
@@ -122,7 +138,9 @@ CREATE TABLE `moderators` (
   `full_name` varchar(60) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `phone` varchar(15) NOT NULL,
+  `faculty_id` int(11) NOT NULL,
   `department_id` int(11) NOT NULL,
+  `level_id` int(11) NOT NULL,
   `reg_date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -130,8 +148,65 @@ CREATE TABLE `moderators` (
 -- Dumping data for table `moderators`
 --
 
-INSERT INTO `moderators` (`id`, `username`, `email`, `password`, `full_name`, `gender`, `phone`, `department_id`, `reg_date`) VALUES
-(1, 'test', 'test@gmail.com', 'password', 'Tester', 'Male', '08123456789', 1, '2020-04-04 15:48:25');
+INSERT INTO `moderators` (`id`, `username`, `email`, `password`, `full_name`, `gender`, `phone`, `faculty_id`, `department_id`, `level_id`, `reg_date`) VALUES
+(1, 'test', 'test@gmail.com', 'test', 'Tester', 'Male', '08123456789', 1, 1, 1, '2020-04-04 15:48:25'),
+(2, 'test2', 'test2@gmail.com', 'test2', 'Tester 2', 'Female', '7575657463', 2, 2, 3, '2020-04-06 14:37:31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `content` varchar(5000) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `level_id` int(11) NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `content`, `category_id`, `department_id`, `level_id`, `date_added`) VALUES
+(1, 'The Test News Title', 'This is some general announcement...', 1, 1, 1, '2020-04-05 10:51:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_categories`
+--
+
+CREATE TABLE `news_categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `category` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `news_categories`
+--
+
+INSERT INTO `news_categories` (`id`, `category`) VALUES
+(1, 'General Announcement'),
+(2, 'Breaking News'),
+(3, 'Assignment'),
+(4, 'Assessment');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `verification_code` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -143,11 +218,26 @@ CREATE TABLE `resources` (
   `id` int(10) UNSIGNED NOT NULL,
   `title` varchar(60) NOT NULL,
   `course_id` int(11) NOT NULL,
+  `faculty_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
   `level_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `description` varchar(2000) NOT NULL,
-  `file` varchar(100) NOT NULL
+  `file` varchar(100) NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `downloads` int(10) UNSIGNED NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `resources`
+--
+
+INSERT INTO `resources` (`id`, `title`, `course_id`, `faculty_id`, `department_id`, `level_id`, `category_id`, `description`, `file`, `date_added`, `downloads`) VALUES
+(3, 'Test 101 Video', 2, 1, 1, 1, 2, 'This is a video for TEST101', 'test-101-video.mp4', '2020-04-05 10:46:47', 0),
+(4, 'Test 101 Document', 2, 1, 1, 1, 4, 'This is a document for TEST101', 'test-101-document.docx', '2020-04-05 10:46:47', 0),
+(5, 'Test 102 Document', 3, 1, 1, 1, 4, 'This is a document for TEST102', 'test-102-document.docx', '2020-04-05 10:46:47', 0),
+(6, 'A Video', 4, 2, 2, 3, 2, 'Im speechless', 'a-video.mp4', '2020-04-06 14:42:05', 0),
+(7, 'A document', 6, 2, 2, 3, 4, 'Im speechless again', 'a-document.xlsx', '2020-04-06 14:43:22', 0);
 
 -- --------------------------------------------------------
 
@@ -165,10 +255,10 @@ CREATE TABLE `resource_categories` (
 --
 
 INSERT INTO `resource_categories` (`id`, `category`) VALUES
-(1, 'materials'),
-(2, 'videos'),
-(3, 'textbooks'),
-(4, 'documents');
+(1, 'Material'),
+(2, 'Video'),
+(3, 'Textbook'),
+(4, 'Document');
 
 --
 -- Indexes for dumped tables
@@ -184,7 +274,9 @@ ALTER TABLE `comments`
 -- Indexes for table `courses`
 --
 ALTER TABLE `courses`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `course_code_2` (`course_code`,`course_title`),
+  ADD KEY `course_code` (`course_code`,`course_title`);
 
 --
 -- Indexes for table `departments`
@@ -214,10 +306,32 @@ ALTER TABLE `moderators`
   ADD UNIQUE KEY `department_id` (`department_id`);
 
 --
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `title` (`title`);
+
+--
+-- Indexes for table `news_categories`
+--
+ALTER TABLE `news_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `verification_code` (`verification_code`);
+
+--
 -- Indexes for table `resources`
 --
 ALTER TABLE `resources`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `title` (`title`);
 
 --
 -- Indexes for table `resource_categories`
@@ -239,19 +353,19 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `faculties`
 --
 ALTER TABLE `faculties`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `levels`
@@ -263,13 +377,31 @@ ALTER TABLE `levels`
 -- AUTO_INCREMENT for table `moderators`
 --
 ALTER TABLE `moderators`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `news_categories`
+--
+ALTER TABLE `news_categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `password_resets`
+--
+ALTER TABLE `password_resets`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `resources`
 --
 ALTER TABLE `resources`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `resource_categories`
