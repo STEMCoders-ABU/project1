a924222948e1d321228f1979bde5b07c5d7e321d
Stem Coders <stemcoders.abu@gmail.com>
2020-05-31 07:00:56 +0100

    Removed useless files

--- dbase.php
+++
@@ -1,7 +0,0 @@
-<?php  
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "registration"
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
